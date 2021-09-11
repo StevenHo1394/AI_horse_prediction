266e6314dfb9fb5ef0428540e4aff376a24697cf
StevenHo1394 <75365123+StevenHo1394@users.noreply.github.com>
2021-09-12 00:18:38 +0800

    Add files via upload
    Corrected the "transform()" from "fit_transform()" when scaling the live data.
    Corrected scaling twice for live data.

--- predict3.py
+++ predict3.py
@@ -9,21 +9,36 @@ Created on Wed Dec  2 18:03:56 2020
 import numpy as np
 import pandas as pd
 
-CURRENT_RACE_DATA = 'new_data/horse_data_20210905_race10.csv'
+PATH_TRAINING_DATA = 'horse_data_train_test.csv'
 
-dataset = pd.read_csv(CURRENT_RACE_DATA)
-X_live = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+CURRENT_RACE_DATA = 'new_data/horse_data_20210908_race2.csv'
 
 import tensorflow as tf
+
+#training data
+dataset = pd.read_csv(PATH_TRAINING_DATA)
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0, shuffle=False)
+
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-X_live = sc.fit_transform(X_live)
+X_train = sc.fit_transform(X_train)
 
+#live data
+dataset = pd.read_csv(CURRENT_RACE_DATA)
+X_live = dataset.iloc[:, :-1].values
+
+#use "transform" for live data
+X_live = sc.transform(X_live) #X_live = sc.fit_transform(X_live)
+
+#loading pretrained model
 model = tf.keras.models.load_model('saved_model/my_model')
 
 # Predicting the Test set results
-y_pred = model.predict( sc.fit_transform(X_live)) 
+y_pred = model.predict(X_live)  #y_pred = model.predict( sc.fit_transform(X_live)) 
 #print("y_pred=", y_pred)
 
 # Predicting the finishing position
